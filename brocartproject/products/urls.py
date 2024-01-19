b89519e6a8ed20183699d34b735cbbc6308dfc27
Sajna Thondikkal <sajnathondikkal@gmail.com>
2024-01-19 14:37:04 -0800

    product list and product details page is completed

--- brocartproject/products/urls.py
+++ brocartproject/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='home'),
-    path('list_products',views.list_products,name="list_products")
+    path('list_products',views.list_products,name="list_products"),
+    path('product_details',views.detail_product,name="detail_product")
 ]
--- brocartproject/products/views.py
+++ brocartproject/products/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def list_products(request):
     return render(request,'products.html')
+
+def detail_product(request):
+    return render(request,'product_detail.html')
