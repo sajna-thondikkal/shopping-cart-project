0789e4b0d19304da344184765427bfff4967cc3b
Sajna Thondikkal <sajnathondikkal@gmail.com>
2024-01-23 15:33:19 -0800

    feature product and latest product is completed

--- brocartproject/products/urls.py
+++ brocartproject/products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='home'),
-    path('list_products',views.list_products,name="list_products"),
-    path('product_details',views.detail_product,name="detail_product")
+    path('product_list',views.list_products,name="list_products"),
+    path('product_details/<pk>',views.detail_product,name="detail_product")
 ]
